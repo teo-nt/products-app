e6d90bd9febc40ca37e7df80e15d3074e35eef9e
Teo <thod_ntonas@hotmail.com>
2024-03-16 20:15:31 +0200

    Correct swagger documentation

--- swagger.js
+++ swagger.js
@@ -11,83 +11,83 @@ exports.options = {
     },
     "openapi": "3.1.0",
     "info": {
-    "version": "1.0.0",
-    "title": "Products CRUD API",
-    "description": "Products project application",
-    "contact": {
-      "name": "API Support",
-      "url": "http://www.example.com",
-      "email": "support@example.com"
-    }
-  },
-  "servers": [
-    {
-        url: 'http://localhost:3000',
-        description: "Local Server"
+        "version": "1.0.0",
+        "title": "Products CRUD API",
+        "description": "Products project application",
+        "contact": {
+            "name": "API Support",
+            "url": "http://www.example.com",
+            "email": "support@example.com"
+        }
     },
-    {
-        url: 'http://www.example.com',
-        description: 'Testing server'
-    }
-  ],
-  'tags': [
-    {
-        'name': 'Users',
-        'description': 'API endpoints for Users'
-    },
-    {
-        'name': 'Products',
-        'description': 'API endpoints for products'
-    },
-    {
-        'name': 'Users and Products',
-        'description': 'API endpoints for Users and their products'
-    }
-  ],
-  'paths': {
-    '/api/users': {
-        'get': {
-            'tags': ['Users'],
-            'description': 'Returns all users',
-            'responses': {
-                '200': {
-                    'description': 'A list of users',
-                    'content': {
-                        'application/json': {
-                            'schema': {
-                                'type': 'array',
-                                'items': {
-                                    '$ref': '#/components/schemas/User'
+    "servers": [
+        {
+            url: 'http://localhost:3000',
+            description: "Local Server"
+        },
+        {
+            url: 'http://www.example.com',
+            description: 'Testing server'
+        }
+    ],
+    'tags': [
+        {
+            'name': 'Users',
+            'description': 'API endpoints for Users'
+        },
+        {
+            'name': 'Products',
+            'description': 'API endpoints for products'
+        },
+        {
+            'name': 'Users and Products',
+            'description': 'API endpoints for Users and their products'
+        }
+    ],
+    'paths': {
+        '/api/users': {
+            'get': {
+                'tags': ['Users'],
+                'description': 'Returns all users',
+                'responses': {
+                    '200': {
+                        'description': 'A list of users',
+                        'content': {
+                            'application/json': {
+                                'schema': {
+                                    'type': 'array',
+                                    'items': {
+                                        '$ref': '#/components/schemas/User'
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
-    },
-    '/api/users/{username}': {
-        'get': {
-            'tags': ['Users'],
-            'parameters': [
-                {
-                    'name': 'username',
-                    'in': 'path',
-                    'required': true,
-                    'description': 'Username of user that we want to find',
-                    'type': 'string'
-                }
-            ],
-            'description': 'Get user with specific username',
-            'response': {
-                '200': {
-                    'description': 'User to find',
-                    'schema': {
-                        '$ref': '#/components/schemas/User'
+        },
+        '/api/users/{username}': {
+            'get': {
+                'tags': ['Users'],
+                'parameters': [
+                    {
+                        'name': 'username',
+                        'in': 'path',
+                        'required': true,
+                        'description': 'Username of user that we want to find',
+                        'type': 'string'
+                    }
+                ],
+                'description': 'Get user with specific username',
+                'responses': {
+                    '200': {
+                        'description': 'User to find',
+                        'schema': {
+                            '$ref': '#/components/schemas/User'
+                        }
                     }
                 }
             }
-        }
-      } 
-  }
+        } 
+    }
 }
